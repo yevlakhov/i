ac44c6e41fd7279f9358152c70293ce6a91d0dd3
Oleg Sysprog <ushatel@gmail.com>
2016-12-23 15:08:18 +0200

    Update tableField.directive.js

--- dashboard-js/client/app/tasks/form-fields/tableField.directive.js
+++ dashboard-js/client/app/tasks/form-fields/tableField.directive.js
@@ -3,6 +3,16 @@ angular.module('dashboardJsApp')
   .directive('tableField', function() {
     return {
       restrict: 'E',
+      controller: ['$scope', function TableFieldController( $scope ) {
+        
+        $scope.broadcast = function(eventName, obj) { 
+
+           $rootScope.$broadcast(eventName, obj); 
+
+           return true;
+        }; 
+        
+      }], 
       templateUrl: 'app/tasks/form-fields/tableField.html'
     };
 });
