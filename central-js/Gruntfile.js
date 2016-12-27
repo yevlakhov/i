9c08cd55ab54a5045e45e1a6200d063b3f952688
lomaka <i.lomachuk@kitsoft.kiev.ua>
2016-12-27 23:23:26 +0200

    New changelist1

--- central-js/Gruntfile.js
+++ central-js/Gruntfile.js
@@ -342,7 +342,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= yeoman.client %>/assets/images',
-          src: '{,*/}*.{png,jpg,jpeg,gif,svg}',
+          src: '{,*/}*.{png,jpg,jpeg,gif}',
           dest: '<%= yeoman.dist %>/public/assets/images'
         }]
       }
@@ -409,7 +409,6 @@ module.exports = function (grunt) {
             'bower_components/**/*',
             'assets/images/{,*/}*.{webp}',
             'assets/fonts/**/*',
-            'app/fonts/**/*',
             'index.html'
           ]
         }, {
