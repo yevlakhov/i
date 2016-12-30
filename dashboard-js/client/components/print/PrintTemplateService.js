fcdb8f8f85b968f72435f72ff3cca817ffee8084
Oleg Sysprog <ushatel@gmail.com>
2016-12-30 16:27:58 +0200

    Updates #1438

--- dashboard-js/client/components/print/PrintTemplateService.js
+++ dashboard-js/client/components/print/PrintTemplateService.js
@@ -43,10 +43,10 @@ angular.module('dashboardJsApp').service('PrintTemplateService', ['tasks', 'Fiel
 
 			    	  var prints = FieldMotionService.getPrintForms(); // form[i].id
 			    	  console.log( " #1438 PrintForms count " + prints.length );
-             
-			    	  angular.forEach ( prints, function(printsItem, printsKey, printsObj ) {
 
-                 if( printsItem.aField_ID.indexOf(form[i].id) > -1 ) { 
+			    	  angular.forEach ( prints, function(printsItem, printsKey, printsObj ) { 
+
+                 if( FieldMotionService.FieldMentioned.inPrintForm( form[i].id )/*_.contains(printsItem.aField_ID, form[i].id)*/ ) { 
 
 		                  angular.forEach( form[i].aRow, function( item, key, obj ) { 
 		
