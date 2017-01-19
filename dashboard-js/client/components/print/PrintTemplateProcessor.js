22a3cf67926919e8b505c1e1c0bfe7af83c2cbfa
skuhtin <sergiy.kuhtin@gmail.com>
2017-01-19 14:32:32 +0200

    update from test

--- dashboard-js/client/components/print/PrintTemplateProcessor.js
+++ dashboard-js/client/components/print/PrintTemplateProcessor.js
@@ -212,48 +212,6 @@ angular.module('dashboardJsApp').factory('PrintTemplateProcessor', ['$sce', 'Aut
          return $sce.trustAsHtml( templateString ); 
       } 
     },
-
-    /** 
-     * function populateTableField (printTemplate, printFormTableObject) 
-     *  Searches printTemplate for [oTableName.sTableField] and replaces 
-     *   with data of specified row of printFormTableObject.nRowIndex 
-     * 
-     * @returns original template with replaced values   
-     * @author Sysprog   
-     */
-    populateTableField: function( printTemplate, printFormTableObject ) { 
-    	
-      var replacement;
-    	var tag;
-      var templateString = ""; 
-
-      if( printTemplate.length ) { 
-         templateString = printTemplate;
-      }
-      else { 
-         templateString = $sce.getTrustedHtml( printTemplate );
-      } 
-
-      if(printFormTableObject.oRow) {
-
-        for ( var fieldIndex in printFormTableObject.oRow.aField ) { 
-
-          var field = printFormTableObject.oRow.aField[fieldIndex];
-          replacement = field.value;
-    		  tag = "["+ printFormTableObject.sTableName + "." + field.id + "]";
-
-      	  templateString = templateString.replace(new RegExp(this.escapeRegExp(tag)), replacement); 
-
-        }
-      } 
-      
-      if( printTemplate.length ) { 
-         return templateString; 
-      }
-      else { 
-         return $sce.trustAsHtml( templateString ); 
-      } 
-    },
     escapeRegExp: function (str) {
       return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
     },
