803e5b93a6bd95f43d1c65cb465cee088b995b8b
lomaka <i.lomachuk@kitsoft.kiev.ua>
2017-01-19 14:50:07 +0200

    fix auth server

--- central-js/client/app/service/print/print.controller.js
+++ central-js/client/app/service/print/print.controller.js
@@ -1,17 +1,22 @@
 angular.module('app').controller('SituationControllerPrint', function ($scope, AdminService, ServiceService, chosenCategory, messageBusService, statesRepository, $rootScope, $sce, $anchorScroll, TitleChangeService, $location) {
-    $scope.bShowSpinner = false;
-    $scope.situation = chosenCategory.aServiceTag_Child[0]
-    $('link').remove();
-    $scope.trustAsHtml = function (string) {
-        if (statesRepository.isKyivCity()) {
-            return $sce.trustAsHtml(string.replace(new RegExp('igov.org.ua', 'g'), 'es.kievcity.gov.ua').replace(new RegExp('iGov.org.ua', 'g'), 'es.kievcity.gov.ua').replace(new RegExp('iGOV.org.ua', 'g'), 'es.kievcity.gov.ua').replace(/\b[Ii][Gg][Oo][Vv]\b/g, 'KievCity')/*.replace(new RegExp('es\.kievcity\.gov\.ua\/wf\/', 'g'), 'igov.org.ua/wf/')*/);
-        } else {
-            return $sce.trustAsHtml(string);
-        }
-    };
-    // $scope.$on('$stateChangeError', function(event, toState) {
-    //   if (toState.resolve) {
-    //     $scope.spinner = false;
-    //   }
-    // });
+  $scope.bShowSpinner = false;
+  $scope.situation = chosenCategory.aServiceTag_Child[0]
+  $('link').remove();
+  $('input').remove()
+  $scope.trustAsHtml = function (string) {
+    if (statesRepository.isKyivCity()) {
+      return $sce.trustAsHtml(string.replace(new RegExp('igov.org.ua', 'g'), 'es.kievcity.gov.ua').replace(new RegExp('iGov.org.ua', 'g'), 'es.kievcity.gov.ua').replace(new RegExp('iGOV.org.ua', 'g'), 'es.kievcity.gov.ua').replace(/\b[Ii][Gg][Oo][Vv]\b/g, 'KievCity')/*.replace(new RegExp('es\.kievcity\.gov\.ua\/wf\/', 'g'), 'igov.org.ua/wf/')*/);
+    } else {
+      return $sce.trustAsHtml(string);
+    }
+  };
+
+  setTimeout(function () {
+    window.print()
+  },100)
+  // $scope.$on('$stateChangeError', function(event, toState) {
+  //   if (toState.resolve) {
+  //     $scope.spinner = false;
+  //   }
+  // });
 });
