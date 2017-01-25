2c1edce55c0edf2f3a7120e4ac5a9682a875a01d
Oleg Sysprog <ushatel@gmail.com>
2017-01-25 19:06:21 +0200

    Update defaults.js

--- public-js/markers/defaults.js
+++ public-js/markers/defaults.js
@@ -196,7 +196,7 @@ angular.module('iGovMarkers')
           aField_ID: ["oPrescription1"], 
           sPatternPath: "/print/kiev/_test_print_table2.html", 
           //sTitleField: "", 
-          asEnumField_ID: {"Condition1": "asType0"}, 
+          asEnumField_ID: {Condition1: "asType0"}, 
           sCondition: " [Condition1] == 'sEnumPr2' && [sPrescriptionEstablished1] == 'встановлено 2'", 
           sName: "Припис про зупинення " 
 
@@ -206,7 +206,7 @@ angular.module('iGovMarkers')
 
             aField_ID: ["oPrescription1"], 
             sPatternPath: "/print/kiev/_test_print_table2.html", 
-            asEnumField_ID: {"Condition1": "asType0"}, 
+            asEnumField_ID: {Condition1: "asType0"}, 
             sCondition: " [Condition1] == 'sEnumPr1' ", 
             sName: "Припис про зупинення both rows та option 2 " 
 
