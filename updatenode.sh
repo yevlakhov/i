294614627d628466bf7bbb76840513be22da685c
lomaka <i.lomachuk@kitsoft.kiev.ua>
2017-04-24 18:10:29 +0300

    fix eds auth 2

--- updatenode.sh
+++ updatenode.sh
@@ -3,6 +3,7 @@ sudo npm install -g n
 sudo n 0.11.14
 node -v
 
+#Service.csv
 #3;_test_ved;100;23;;;;0;Тестова служба
 #57;_test_send_user;100;23;;;;0;Тестова служба
 #116;_test_customStyle;100;23;;;;0;Тестова служба
@@ -53,3 +54,55 @@ node -v
 #2502;_test_skuhtin;100;23;[*];;;0;Тестова служба
 #2513;_test_table_req;100;23;;;;0;Тестова служба
 #679;_test_Видача довідки щодо реєстрації/зняття з реєстрації місця проживання фізичноїk особи;100;23;;;;0;Місцеві органи влади
+
+
+
+
+#ServiceData.csv
+#362;3;1;1;NULL;4;{"processDefinitionId":"_test_ved:1:1"};;false;1;false;;BankID,EDS;0
+#121;57;50001;NULL;1;4;{"processDefinitionId":"_test_send_user:1:1"};;false;1;true;;BankID,EDS,KK;0
+#741;116;NULL;NULL;NULL;4;{"processDefinitionId":"_test_customStyle:1:1"};;false;1;true;;BankID,EDS;0
+#127;670;50001;NULL;1;4;{"processDefinitionId":"_test_mailer_ext:1:1"};;false;1;false;;BankID,email,EDS,KK;0
+#128;671;NULL;NULL;NULL;4;{"processDefinitionId":"_test_dependence_form:1:1"};;false;1;false;;BankID,email,EDS,KK;0
+#129;672;NULL;NULL;NULL;4;{"processDefinitionId":"_test_dnepr_mvd_1_auto:1:1"};;false;1;false;;BankID,email,EDS,KK;0
+#130;673;468;468;NULL;4;{"processDefinitionId":"_test_print_form:1:1"};;false;225;true;;BankID,email,EDS,KK;0
+#131;674;NULL;NULL;NULL;4;{"processDefinitionId":"_test_liqpay:1:1"};;false;1;false;;BankID,email,EDS,KK;0
+#151;688;NULL;NULL;NULL;4;{"processDefinitionId":"_test_Country:1:1"};;false;1;false;;BankID,email,EDS,KK;0
+#160;155;NULL;NULL;NULL;4;{"processDefinitionId":"_doc_test_justice_setTasks:1:1"};;false;1;true;;BankID,EDS;0
+#184;352;1;1;NULL;4;{"processDefinitionId":"_test_dnepr_mvk_291_common:1:1"};;false;1;false;;BankID,email,EDS;0
+#240;220;50001;NULL;1;4;{"processDefinitionId":"_test_file_listeners:1:1"};;false;128;true;;BankID,EDS;0
+#260;280;NULL;NULL;NULL;4;{"processDefinitionId":"_test_subProcess:1:1"};;false;1;true;;BankID,EDS;0
+#290;720;1;1;NULL;4;{"processDefinitionId":"_test_fields_bankid:1:151"};;false;1;false;;BankID,email,EDS,KK;0
+#349;751;1;1;NULL;4;{"processDefinitionId":"_test_datepicker:1:1"};;false;1;false;;BankID,email,EDS,KK;0
+#353;755;NULL;NULL;NULL;4;{"processDefinitionId":"_test_mailer:1:1"};;false;1;false;;BankID,email,EDS,KK;0
+#358;756;1;1;NULL;4;{"processDefinitionId":"_test_queue_cancel:1:1"};;false;1;false;;BankID,email,EDS,KK;0
+#359;359;50001;NULL;1;4;{"processDefinitionId":"_test_dictionary_script:1:1"};;false;1;true;;BankID,EDS;0
+#360;757;NULL;NULL;NULL;4;{"processDefinitionId":"_test_type_HTML:1:1"};;false;1;false;;BankID,email,EDS,KK;0
+#372;769;1;1;NULL;4;{"processDefinitionId":"_test_smart_print_form:1:238"};;false;1;false;;BankID,email,EDS,KK;0
+#392;784;1;1;NULL;4;{"processDefinitionId":"_test_autosend_printform:1:238"};;false;1;false;;BankID,email,EDS,KK;0
+#395;785;1;1;NULL;4;{"processDefinitionId":"_test_sID_UA:1:238"};;false;1;false;;BankID,email,EDS,KK;0
+#414;1382;1;1;NULL;4;{"processDefinitionId":"_test_enum_usertask:1:1"};;false;1;false;;BankID,email,EDS,KK;0
+#418;785;10;10;NULL;4;{"processDefinitionId":"_test_sID_UA:1:238"};;false;1;false;;BankID,email,EDS,KK;0
+#424;1389;NULL;NULL;NULL;4;{"processDefinitionId":"_testform_autosign_ECP:1:1"};;false;1;true;;BankID,email,EDS,KK;0
+#450;1394;NULL;NULL;NULL;4;{"processDefinitionId":"_test_gris:1:1"};;false;1;false;;BankID,email,EDS,KK;0
+#631;1451;NULL;NULL;NULL;4;{"processDefinitionId":"_test_selectors:1:1"};;false;284;false;;BankID,email,EDS,KK;0
+#742;130;NULL;NULL;NULL;4;{"processDefinitionId":"_doc_test_justice_incoming:1:1"};;false;1;true;;BankID,EDS;0
+#883;154;1;1;NULL;4;{"processDefinitionId":"_test_UKR_DOC:1:1"};;false;1;false;;BankID,email,EDS;0
+#1177;119;1;1;NULL;4;{"processDefinitionId":"_test_corezoid:1:1"};;false;1;false;;BankID,email,EDS;0
+#1355;669;1;1;NULL;4;{"processDefinitionId":"_test_SED_SwinEd_1:1:1"};;false;1;false;;BankID,email,EDS;0
+#2090;670;50002;NULL;1;4;{"processDefinitionId":"_test_mailer_ext:1:1"};;false;1;false;;BankID,email,EDS,KK;0
+#2091;670;50003;NULL;1;4;{"processDefinitionId":"_test_mailer_ext:1:1"};;false;1;false;;BankID,email,EDS,KK;0
+#2970;1514;NULL;NULL;NULL;4;{"processDefinitionId":"_test_group_pattern:1:1"};;false;1;true;;BankID,EDS;0
+#2972;1515;NULL;NULL;NULL;4;{"processDefinitionId":"_test_sID_SubjectActionKVED:1:1"};;false;1;true;;BankID,EDS;0
+#3000;1532;NULL;NULL;NULL;4;{"processDefinitionId":"_test_liqpay_startform:1:1"};;false;1;false;;BankID,email,EDS,KK;0
+#3009;1538;50001;NULL;1;4;{"processDefinitionId":"_test_SpecInspection_965:1:1"};;false;761;true;;BankID,EDS;0
+#3077;1539;1;1;NULL;4;{"processDefinitionId":"_test_queue_DMS:1:1"};;false;1;false;;BankID,email,EDS,KK;0
+#3108;1542;50001;NULL;1;4;{"processDefinitionId":"_test_new_dashboard:1:58"};;false;306;false;;BankID,EDS;0
+#3171;1543;NULL;NULL;NULL;4;{"processDefinitionId":"_testform_autosign_ECP_new:1:1"};;false;1;true;;BankID,email,EDS,KK;0
+#3198;1550;NULL;NULL;NULL;4;{"processDefinitionId":"_test_field_checkbox:1:1"};;false;1;true;;BankID,email,EDS,KK;0
+#3211;732;1;1;NULL;4;{"processDefinitionId":"_test_PDF_attached:1:1"};;false;1;false;;BankID,email,EDS;0
+#3270;1549;50008;NULL;8;4;{"processDefinitionId":"_test_DFS_1374:1:1"};;false;286;true;;BankID,EDS;0
+#5000;2500;NULL;NULL;NULL;4;{"processDefinitionId":"_test_print_table:1:1"};;false;1;true;;BankID;0
+#5001;2501;NULL;NULL;NULL;4;{"processDefinitionId":"_test_choose_employee:1:1"};;false;1;true;;BankID;0
+#5002;2502;50005;NULL;5;4;{"processDefinitionId":"_test_skuhtin:1:1"};;false;1;true;;BankID;8
+#5016;2513;NULL;NULL;NULL;4;{"processDefinitionId":"_test_table_req:1:1"};;false;1;true;;BankID;0
